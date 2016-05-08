9ebfdd2f47553ddf797e484bd5b244f2c14c5837
baumar <baumann_ryan@cat.com>
2016-05-07 20:12:38 -0500

    initial commit of proof of concept

---
+++ testserver.py
@@ -0,0 +1,21 @@
+from flask import Flask, request, send_from_directory, url_for
+from flask.ext.cors import CORS
+app = Flask(__name__, static_url_path='/data')
+CORS(app)
+
+
+
+@app.route('/')
+def root():
+  return app.send_static_file('index.html')
+
+@app.route('/<path:path>')
+def static_proxy(path):
+  # send_static_file will guess the correct MIME type
+  return app.send_static_file(path)
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=8888)
+
